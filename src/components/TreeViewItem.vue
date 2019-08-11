f25d9088278ad6b56f93d6e020da2a72910d40ba
Andy0414 <pjh8667@naver.com>
2019-08-11 13:11:20 +0900

    UPDATE auto open

--- src/components/TreeViewItem.vue
+++ src/components/TreeViewItem.vue
@@ -4,14 +4,15 @@
 			<span v-html="text"></span>
 			<span class="folder">({{isOpen ? "－":"＋"}})</span>
 		</h4>
-        <ul class="tab" v-if="isOpen">
+		<ul class="tab" v-if="isOpen">
 			<tree-view-item
 				:highlight="highlight"
 				:data-index="index"
 				:data="item"
 				v-for="(item,index) in data.children"
 				:key="item.name"
-			></tree-view-item></ul>
+			></tree-view-item>
+		</ul>
 	</li>
 	<li class="listitem" ref="listitem" v-else>
 		<span v-html="text"></span>
@@ -39,10 +40,20 @@ export default Vue.extend({
 	},
 	created() {
 		this.isFolder = this.data.hasOwnProperty("children");
+
 		this.setText(this.highlight);
 	},
 	watch: {
 		highlight(value) {
+			if (this.isFolder && value != "") {
+				this.isOpen =
+					this.data.children.findIndex(
+						x => x.name.indexOf(value) != -1
+					) != -1 || this.data.name.indexOf(value) != -1;
+			}
+			else{
+				this.isOpen = false
+			}
 			this.setText(value);
 		}
 	},
@@ -59,7 +70,8 @@ export default Vue.extend({
 			el.style.transitionDelay = el.dataset.index * 50 + "ms";
 		},
 		leave(el, done) {
-			el.style.transitionDelay = (this.data.children.length-1-el.dataset.index) * 50 + "ms";
+			el.style.transitionDelay =
+				(this.data.children.length - 1 - el.dataset.index) * 50 + "ms";
 		}
 	},
 	computed: {}
@@ -78,16 +90,15 @@ export default Vue.extend({
 
 	display: flex;
 	flex-direction: column;
-    
-    font-size: 0.9em;
+
+	font-size: 0.9em;
 }
 .listitem {
-    margin-top: 5px;
+	margin-top: 5px;
 	display: block;
 	overflow: visible;
 }
 .folder {
 	font-size: 0.8em;
 }
-
 </style>
